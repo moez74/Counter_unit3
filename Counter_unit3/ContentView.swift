043768d3df1b55705406959988b4333fcdd468e0
Moez Rehman <moez.rehman.gcu@gmail.com>
2023-06-24 14:22:56 +0400

    Initial Commit

---
+++ Counter_unit3/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Counter_unit3
+//
+//  Created by Moez Rehman on 6/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+//    @State private var counter = 0
+//    @State private var message = "Some Message"
+    @AppStorage("counter") var counter = 0
+    @AppStorage("message") var message = ""
+    
+    var body: some View {
+        VStack {
+            Text("\(counter) cookies eaten")
+            
+            Button {
+                print("Bye bye cookie!")
+                counter += 1
+                
+                if counter >= 20 {
+                    message = "Don't eat too many cookies ☹️"
+                }
+                if counter >= 30 {
+                    message = "What are you doing??? 😰"
+                }
+                if counter >= 40 {
+                    message = "Oh nooooooooooo 😱"
+                }
+            } label: {
+                Text("OM NOM NOM")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .shadow(color: .white.opacity(0.3),
+                    radius: 10, x: 0.0, y: 0.0)
+            
+            Text(message)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Counter_unit3/Counter_unit3App.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_unit3App.swift
+//  Counter_unit3
+//
+//  Created by Moez Rehman on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Counter_unit3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
